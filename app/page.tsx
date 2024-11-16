c2cdc46e4bddc46d2dd8e7fd8842ee24cd287938
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-11-15 21:54:31 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Neon Store - Your Next-Gen Cloud Storage</h1>
+      <h1 className={"text-brand-100 text-center text-3xl font-bold"}>
+        Neon Store - Your Next-Gen Cloud Storage
+      </h1>
     </div>
   );
 }
