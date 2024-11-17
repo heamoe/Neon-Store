7458c663ad8bdcbb295080b00db317fb6b6fe14d
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-11-16 21:58:32 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1 className={"text-brand-100 text-center text-3xl font-bold"}>
+      <h1 className={"text-center text-3xl font-bold text-brand-100"}>
         Neon Store - Your Next-Gen Cloud Storage
       </h1>
     </div>
