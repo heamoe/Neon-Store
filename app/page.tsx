384c7de0b0d0776e929a6ad92a3f633d928d40a0
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-11-16 22:40:35 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className={"center"}>
+    <div className={"flex-center h-screen"}>
       <h1 className={"h1"}>Neon Store - Your Next-Gen Cloud Storage</h1>
     </div>
   );
