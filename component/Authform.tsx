79c82b212f927141fce06ca650556ea382cf23a1
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-11-20 10:00:17 -0500

    update

--- component/Authform.tsx
+++
@@ -1,5 +0,0 @@
-type FormType = "login" | "signup";
-const Authform = ({ type }: { type: FormType }) => {
-  return <div></div>;
-};
-export default Authform;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,5 @@
+type FormType = "login" | "signup";
+const AuthForm = ({ type }: { type: FormType }) => {
+  return <div></div>;
+};
+export default AuthForm;
