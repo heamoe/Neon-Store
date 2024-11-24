ea70a056f905a23586683810531a09f16788dec0
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-11-23 21:37:55 -0500

    update

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,15 +5,18 @@ import { ID, Query } from "node-appwrite";
 import { avatarPlaceholderUrl } from "@/constants";
 import { parseStringify } from "@/lib/utils";
 
-const getUsersByEmail = async (email: string) => {
-  const { database } = await createAdminClient();
-  const result = await database.listDocuments(
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
     appwriteConfig.databaseId,
     appwriteConfig.usersCollectionId,
     [Query.equal("email", [email])],
   );
+
   return result.total > 0 ? result.documents[0] : null;
 };
+
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
   throw error;
@@ -36,7 +39,7 @@ export const createAccount = async ({
   fullName: string;
   email: string;
 }) => {
-  const existingUser = await getUsersByEmail(email);
+  const existingUser = await getUserByEmail(email);
   const accountId = await sendEmailOTP({ email });
   if (!accountId) throw new Error("Failed to send an OTP");
 
