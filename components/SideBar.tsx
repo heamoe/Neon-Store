9f76534b9d630b8c8c515cc1264209cdcf8bd625
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-03 15:32:03 -0500

    update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const SideBar = () => {
-    return (
-        <div>SideBar</div>
-    )
+    return <aside className="sidebar">
+        <Link href={"/"}>
+            <Image src="/assets/icons/logo-full-brand.svg" alt="logo" width={160} height={50} className="hidden h-auto lg:block" />
+            <Image src="/assets/icons/logo-brand.svg" alt="logo" width={52} height={52} className=" lg:hidden" />
+        </Link>
+    </aside>
 }
 export default SideBar
