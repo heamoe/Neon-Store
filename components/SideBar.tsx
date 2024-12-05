1a36663b27e227c819b1d958d9e65008e8613212
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-05 16:31:41 -0500

    update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 const SideBar = () => {
     return <aside className="sidebar">
         <Link href={"/"}>
-            <Image src="/assets/icons/logo-full-brand.svg" alt="logo" width={160} height={50} className="hidden h-auto lg:block" />
-            <Image src="/assets/icons/logo-brand.svg" alt="logo" width={52} height={52} className=" lg:hidden" />
+            <Image src="/assets/icons/logo-full-brand.svg" alt="desktop logo" width={160} height={50} className="hidden h-auto lg:block" />
+            <Image src="/assets/icons/logo-brand.svg" alt="mobile logo" width={52} height={52} className=" lg:hidden" />
         </Link>
     </aside>
 }
