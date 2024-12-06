ec3122d394be61a2c248af5cdd9f71c8ec006e84
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-06 16:40:20 -0500

    update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { navItems } from "@/constants";
+import {avatarPlaceholderUrl, navItems} from "@/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
@@ -72,7 +72,7 @@ const Sidebar = ({ fullName, avatar, email }: Props) => {
 
             <div className="sidebar-user-info">
                 <Image
-                    src={avatar}
+                    src={avatarPlaceholderUrl}
                     alt="Avatar"
                     width={44}
                     height={44}
