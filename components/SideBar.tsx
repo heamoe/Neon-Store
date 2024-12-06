f898152e6dad240e69c5a369631416449e23d2ff
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-06 15:18:44 -0500

    update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,13 +1,31 @@
+"use client"
+
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import {navItems} from "@/constants";
+import {usePathname} from "next/navigation";
 
 const SideBar = () => {
+    const {pathname} = usePathname();
     return <aside className="sidebar">
         <Link href={"/"}>
-            <Image src="/assets/icons/logo-full-brand.svg" alt="desktop-logo" width={160} height={50} className="hidden h-auto lg:block" />
-            <Image src="/assets/icons/logo-brand.svg" alt="mobile-logo" width={52} height={52} className=" lg:hidden" />
+            <Image src="/assets/icons/logo-full-brand.svg" alt="desktop-logo" width={160} height={50}
+                   className="hidden h-auto lg:block"/>
+            <Image src="/assets/icons/logo-brand.svg" alt="mobile-logo" width={52} height={52} className=" lg:hidden"/>
         </Link>
+        <nav className="sidebar-nav">
+            <ul className="flex flex-1 flex-col gap-6">
+                {navItems.map(({url, name, icon}) => (
+                    <Link key={name} href={url}>
+                        <li>
+                            <Image src={icon} alt={name} width={24} height={24}/>
+                            <p>{name}</p>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+        </nav>
     </aside>
 }
 export default SideBar
