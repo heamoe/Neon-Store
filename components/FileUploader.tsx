c1e9f0adf52925ba5b33830fee9480999e5a292d
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-18 19:39:47 -0500

    Enhance FileUploader component with drag-and-drop functionality using react-dropzone; update package.json and package-lock.json to include react-dropzone dependency

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+'use client';
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Button } from './ui/button';
+import Image from "next/image";
+import { cn, } from "@/lib/utils";
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+    const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
-const FileUploader = () => {
-    return (
-        <div>FileUploader</div>
-    )
+  return (
+    <div {...getRootProps()} className='cursor-pointer'>
+      <input {...getInputProps()} />
+       <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload"
+          width={24}
+          height={24}
+        />{" "}
+        <p>Upload</p>
+      </Button>
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 }
 export default FileUploader
