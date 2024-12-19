b45266e00947f6fa147dae09bbf3a125af5df211
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-19 17:44:47 -0500

    Refactor FileUploader component to manage file uploads with state; add file preview and removal functionality. Enhance utility functions for file type detection and URL conversion.

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,24 +1,26 @@
-'use client';
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
-import { Button } from './ui/button';
+"use client";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
 import Image from "next/image";
-import { cn, } from "@/lib/utils";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import { get } from "http";
+import Thumbnail from "./Thumbnail";
 interface Props {
   ownerId: string;
   accountId: string;
   className?: string;
 }
 const FileUploader = ({ ownerId, accountId, className }: Props) => {
-    const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback(async(acceptedFiles : File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div {...getRootProps()} className='cursor-pointer'>
+    <div {...getRootProps()} className="cursor-pointer">
       <input {...getInputProps()} />
-       <Button type="button" className={cn("uploader-button", className)}>
+      <Button type="button" className={cn("uploader-button", className)}>
         <Image
           src="/assets/icons/upload.svg"
           alt="upload"
@@ -27,12 +29,53 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
         />{" "}
         <p>Upload</p>
       </Button>
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+
+            function handleRemoveFile(e: React.MouseEvent<HTMLImageElement, MouseEvent>, name: string): void {
+              throw new Error("Function not implemented.");
+            }
+
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item"
+              >
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className="preview-item-name">
+                    {file.name}
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      width={80}
+                      height={26}
+                      alt="Loader"
+                    />
+                  </div>
+                </div>
+
+                <Image
+                  src="/assets/icons/remove.svg"
+                  width={24}
+                  height={24}
+                  alt="Remove"
+                  onClick={(e) => handleRemoveFile(e, file.name)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
-  )
-}
-export default FileUploader
+  );
+};
+export default FileUploader;
