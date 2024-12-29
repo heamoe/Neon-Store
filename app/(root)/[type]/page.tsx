9905954dddaa99a859466d76734985f5deb968d6
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-12-29 15:19:24 -0500

    Update page component to handle dynamic type parameter and improve structure
    - Refactor page component to accept `params` and extract `type` for dynamic rendering.
    - Enhance the JSX structure with a container and section for better styling and layout.
    - Corrected the closing tag for the `h1` element to ensure proper HTML syntax.

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+  return <div className="page-container">
+    <section className="w-full">
+        <h1 className="h1 capitalize text-center">{type}></h1>
+    </section>
+  </div>;
+};
 
-export default page
+export default page;
