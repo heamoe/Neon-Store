316c883cdf06152d16ea7801cccc8c5fb10af9d9
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2025-01-07 14:27:21 -0500

    Enhance file actions: add createQueries function for improved file retrieval based on user ownership and email; update error handling in getFiles function to throw an error if the user is not found; refine import statements for better clarity.

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -2,7 +2,7 @@
 import { createAdminClient } from "@/lib/appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appwriteConfig } from "@/lib/appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 import { getCurrentUser } from "@/lib/actions/user.actions";
@@ -59,12 +59,27 @@ export const uploadFile = async ({
     handleError(error, "Failed to upload file");
   }
 };
-
+const createQueries = (currentUser: Models.Document) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", currentUser.$id),
+      Query.equal("users", currentUser.email),
+    ]),
+  ]
+  return queries;
+};
 export const getFiles = async () => {
   const { databases } = await createAdminClient();
   try {
     const currentUser = await getCurrentUser();
-    if(!currentUser){}
+    if(!currentUser){throw new Error("User not found")}
+    const queries = createQueries(currentUser);
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries
+    );
+    return parseStringify(files);
   } catch (error) {
     handleError(error, "Failed to get files");
   }
