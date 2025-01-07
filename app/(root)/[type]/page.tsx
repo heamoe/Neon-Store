0789ff38d9c0b9d4bedf0c9fd8e2bee031dd9896
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2025-01-06 20:08:20 -0500

    Refactor page component to enhance structure and dynamic rendering of type parameter; add total size section and sorting options for improved layout and user experience.

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -2,11 +2,21 @@ import React from "react";
 
 const page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
-  return <div className="page-container">
-    <section className="w-full">
+  return (
+    <div className="page-container">
+      <section className="w-full">
         <h1 className="h1 capitalize text-center">{type}</h1>
-    </section>
-  </div>;
+        <div className="total-size-section">
+          <p className="body-1">
+            Total Size: <span className="h5">0 MB</span>
+          </p>
+          <div className="sort-container">
+            <p className="body-1 hidden sm:block text-light-200">Sort By:</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default page;
