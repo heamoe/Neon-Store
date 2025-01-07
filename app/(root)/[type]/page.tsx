7e6651e6e261742c2d337d7abc42d483e2a78a32
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2025-01-07 14:58:59 -0500

    Integrate Card component in page: replace file ID display with Card component for enhanced file representation in the UI.

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -2,6 +2,7 @@ import Sort from "@/components/Sort";
 import React from "react";
 import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
+import Card from "@/components/Card";
 
 const page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
@@ -23,7 +24,7 @@ const page = async ({ params }: SearchParamProps) => {
       {files.total > 0 ? (
         <section className="file-list">
           {files.documents.map((file:Models.Document)=>(
-            <h1 key={files.$id} className="h1">{file.$id}</h1>
+            <Card key={file.$id} file={file} />
           ))}
         </section>
       ) : (
