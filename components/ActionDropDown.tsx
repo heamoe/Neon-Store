3a328efaa158eeeba372edec358288a3ce2c123f
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2025-01-09 16:15:15 -0500

    Update package dependencies and enhance ActionDropDown component with Radix UI dropdown integration. Upgraded @radix-ui/react-dialog to version 1.1.4 and added @radix-ui/react-dropdown-menu version 2.1.4 for improved UI functionality. Refactored ActionDropDown to utilize new dropdown components for better user interaction.

--- components/ActionDropDown.tsx
+++ components/ActionDropDown.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const ActionDropDown = () => {
   return (
-    <div>ActionDropDown</div>
-  )
-}
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
+};
 
-export default ActionDropDown
+export default ActionDropDown;
