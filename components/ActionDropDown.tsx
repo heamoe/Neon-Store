757f5af571338d1ae4ab44a94ff2bf0be37f4418
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2025-01-09 17:09:50 -0500

    Refactor ActionDropDown component to accept file prop and enhance dropdown functionality. Integrate ActionDropDown in Card component for improved file action accessibility. Update UI elements for better user interaction and display file name in dropdown.

--- components/ActionDropDown.tsx
+++ components/ActionDropDown.tsx
@@ -1,12 +1,6 @@
-import React from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+"use client";
+import React, { useState } from "react";
+import { Dialog } from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,19 +9,37 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+import { actionsDropdownItems } from "@/constants";
+import { set } from "zod";
 
-const ActionDropDown = () => {
+const ActionDropDown = ({file}:{file:Models.Document}) => {
+  const [isModelOpen, setisModelOpen] = useState(false);
+  const [isDropDownOpen, setisDropDownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType|null>(null);
   return (
-    <Dialog>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+    <Dialog open={isModelOpen} onOpenChange={() => setisModelOpen}>
+      <DropdownMenu open={isDropDownOpen} onOpenChange={setisDropDownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image src="/assets/icons/dots.svg" alt="dots" width={34} height={34}/>
+        </DropdownMenuTrigger>
+
         <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel className="max-w-[200px] truncate">{file.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
+          {actionsDropdownItems.map((actionItem)=> (
+            <DropdownMenuItem
+              key={actionItem.value}
+              className="shad-dropdown-item"
+              onClick={() => {
+                setAction(actionItem);
+                if(['rename','share','delete','details'].includes(actionItem.value)){
+                  setisModelOpen(true);
+                }
+              }}
+            ></DropdownMenuItem>
+          ))}
         </DropdownMenuContent>
       </DropdownMenu>
     </Dialog>
